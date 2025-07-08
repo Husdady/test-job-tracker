18d2a48e7e0116b86698dc7f3f4aa4f55701df19
Husdady Mena <husdadymena@gmail.com>
2025-07-08 10:00:17 -0500

    Save changes

--- src/App.js
+++ src/App.js
@@ -1,93 +1,89 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 //import { getData, postData } from '../src/services/api';
-import { getJobData, postJobData } from './services/api.v1';
-import { useParams , useLocation} from 'react-router-dom';
- 
+import { getJobData, postJobData } from "./services/api.v1";
+import { useParams, useLocation } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 // App
-import Header from './components/Header'
-import NavBar from './components/NavBar'
-import JobInfo from './components/JobInfo'
-import StartJob from './components/StartJob';
-import EndJob from './components/EndJob';
-import JobReview from './components/JobReview';
-import Payment from './components/Payment';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import JobInfo from "./components/JobInfo";
+import StartJob from "./components/StartJob";
+import EndJob from "./components/EndJob";
+import JobReview from "./components/JobReview";
+import Payment from "./components/Payment";
+import PhotoCameraOverlay from "./components/startjob/PhotoCameraOverlay";
 
-import {formatDate} from './utils/misc';
+import { formatDate } from "./utils/misc";
 
-let locationStatus  = 0; 
- 
+let locationStatus = 0;
+
 function App() {
   const activeWindow = useLocation();
   const { idJob } = useParams();
-  const [location, setLocation] = useState(null)
-  const [error, setError] = useState(false)
- // 0 = fail, 1=done, 2=need access, 3=not supported
- const [idFromUrl, setIdFromUrl] = useState(null);
-
- useEffect(() => {
-  if (!idJob) return;
-
-  console.log('ID desde URL:', idJob);
-  // Aquí puedes hacer tu fetch a la API usando ese id
-}, [idJob]);
- 
- useEffect(() => {
-  getJobData(123)
-    .then(data => console.log('GET OK:', data))
-    .catch(err => console.error('GET Fail:', err));
-
-  postJobData({ name: 'David', status: 'started' })
-    .then(data => console.log('POST OK:', data))
-    .catch(err => console.error('POST Fail:', err));
-}, []);
-
-
-
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(false);
+  // 0 = fail, 1=done, 2=need access, 3=not supported
+  const [idFromUrl, setIdFromUrl] = useState(null);
 
   useEffect(() => {
-    console.log(1)
+    if (!idJob) return;
+
+    console.log("ID desde URL:", idJob);
+    // Aquí puedes hacer tu fetch a la API usando ese id
+  }, [idJob]);
+
+  useEffect(() => {
+    getJobData(123)
+      .then((data) => console.log("GET OK:", data))
+      .catch((err) => console.error("GET Fail:", err));
+
+    postJobData({ name: "David", status: "started" })
+      .then((data) => console.log("POST OK:", data))
+      .catch((err) => console.error("POST Fail:", err));
+  }, []);
+
+  useEffect(() => {
+    console.log(1);
     //const params = new URLSearchParams(activeWindow.search);
     //const id = params.get('idJob'); // o el nombre que uses en la URL
-     //alert(id)
+    //alert(id)
     //if (id) setIdFromUrl(id);
-    
   }, [activeWindow.search]);
 
   const getLocation = () => {
-    console.log(2)
+    console.log(2);
     if (!navigator.geolocation) {
-      locationStatus = 3
+      locationStatus = 3;
       //setError("Geolocation not supported")
-      return
-    }    
+      return;
+    }
     navigator.geolocation.getCurrentPosition(
       (position) => {
         setLocation({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-        })
-        locationStatus = 1
-        setError(false)
+        });
+        locationStatus = 1;
+        setError(false);
       },
       (err) => {
         if (err.code === 1) {
           // PERMISSION_DENIED
-          locationStatus = 2
+          locationStatus = 2;
           //setError("We couldn't access your location. Please enable location services in your device settings to continue.")
         } else {
-          locationStatus=0
+          locationStatus = 0;
           //setError("Error getting location.")
         }
       }
-    )
-  }
+    );
+  };
 
   useEffect(() => {
-    getLocation()
+    getLocation();
     if (!idFromUrl) return;
-/*
+    /*
     getData(idFromUrl)
     .then((data) => {
       setJobInfoData((prev) => ({
@@ -105,89 +101,112 @@ function App() {
       }));
     })
     .catch(err => console.error(err));*/
+  }, [idFromUrl]);
 
-    
-  }, [idFromUrl])
-
-  //  onStatusChange={updateIconStatus} 
+  //  onStatusChange={updateIconStatus}
   const confirmLocation = () => {
- 
     const formatted = formatDate();
     //setDateConfirm(formatted);
     setJobInfoData({
       ...jobInfoData,
-      dateConfirm : formatted,
-      locationBrowser : location
-    })
+      dateConfirm: formatted,
+      locationBrowser: location,
+    });
     completarPaso(2);
-   }
-  // steps 
+  };
+  // steps
   const [currentStep, setCurrentStep] = useState(1);
-  const [enabledSteps, setEnabledSteps] = useState([1,5,4]);
+  const [enabledSteps, setEnabledSteps] = useState([1, 5, 4]);
 
   const completarPaso = (num) => {
-    setEnabledSteps(prev => [...new Set([...prev, num])]);
+    setEnabledSteps((prev) => [...new Set([...prev, num])]);
     setCurrentStep(num);
   };
 
-  const confirmStarJOb =  () => {
-    completarPaso(3)
-  }
+  const confirmStarJOb = () => {
+    completarPaso(3);
+  };
 
-  const confirmEndJOb =  () => {
-    completarPaso(4)
-  }
- // JobInfo
- const [jobInfoData, setJobInfoData] = useState({
+  const confirmEndJOb = () => {
+    completarPaso(4);
+  };
+  // JobInfo
+  const [jobInfoData, setJobInfoData] = useState({
     idJob: "XXXX",
-    name : "David Room",
-    address : "Jr comercio 987, Cañete, Cerro Azul 9898",
+    name: "David Room",
+    address: "Jr comercio 987, Cañete, Cerro Azul 9898",
     unit: "AB987",
-    locationn : {
+    locationn: {
       lat: -98987897,
       lng: +898798798,
     },
-    cleaningType : "Cleaner All",
-    scheduled : "Friday, December 27th, 2024",
-    instructions : "Enter through the back gate using code 4832. Pets on premises.",
+    cleaningType: "Cleaner All",
+    scheduled: "Friday, December 27th, 2024",
+    instructions:
+      "Enter through the back gate using code 4832. Pets on premises.",
     dateConfirm: null,
-    locationBrowser: null 
- })
-  
+    locationBrowser: null,
+  });
+
   // StartJOb
   const [startJobData, setStartJobData] = useState({
     photos: [],
     isConfirmed: false,
-    dateConfirm:null,
+    dateConfirm: null,
     option: 0,
-    videos:[]
+    videos: [],
   });
- // end job
+  // end job
   const [endJobData, setEndJobData] = useState({
     photos: [],
     isConfirmed: false,
-    dateConfirm:null,
+    dateConfirm: null,
     option: 0,
-    videos:[]
+    videos: [],
   });
   // STATUS PAYMENT
-  const [paymentStatus, setPaymentStatus]  = useState('I'); //I(inprogress) P (processing) D (done paid) 
-  const [department, setDepartment]  = useState(jobInfoData.name + ' - ' + jobInfoData.unit);
-
+  const [paymentStatus, setPaymentStatus] = useState("I"); //I(inprogress) P (processing) D (done paid)
+  const [department, setDepartment] = useState(
+    jobInfoData.name + " - " + jobInfoData.unit
+  );
 
   return (
     <div className="App">
-       <Header data={jobInfoData} />
-       <NavBar 
-          currentStep={currentStep}
-          enabledSteps={enabledSteps}
-          onStepChange={setCurrentStep}  />
+      <Header data={jobInfoData} />
 
-       {currentStep === 1 && <JobInfo locationStatus={locationStatus}  onStatusChange={confirmLocation} data={jobInfoData} />}
-       {currentStep === 2 && <StartJob data={startJobData} setData={setStartJobData} startJobConfirmed={confirmStarJOb} />}
-       {currentStep === 3 && <EndJob data={endJobData} setData={setEndJobData} startJobConfirmed={confirmEndJOb} />}
-       {currentStep === 4 && <JobReview statusJob={paymentStatus} dept={department} />}
-       {currentStep === 5 && <Payment statusJob={paymentStatus}/>}
+      <NavBar
+        currentStep={currentStep}
+        enabledSteps={enabledSteps}
+        onStepChange={setCurrentStep}
+      />
+
+      <PhotoCameraOverlay />
+
+      {currentStep === 1 && (
+        <JobInfo
+          locationStatus={locationStatus}
+          onStatusChange={confirmLocation}
+          data={jobInfoData}
+        />
+      )}
+      {currentStep === 2 && (
+        <StartJob
+          data={startJobData}
+          setData={setStartJobData}
+          startJobConfirmed={confirmStarJOb}
+        />
+      )}
+      {currentStep === 3 && (
+        <EndJob
+          data={endJobData}
+          setData={setEndJobData}
+          startJobConfirmed={confirmEndJOb}
+        />
+      )}
+      {currentStep === 4 && (
+        <JobReview statusJob={paymentStatus} dept={department} />
+      )}
+      {currentStep === 5 && <Payment statusJob={paymentStatus} />}
     </div>
   );
 }
