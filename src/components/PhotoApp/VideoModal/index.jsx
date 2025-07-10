0fb5ad26490e77bc35c6aa16e0d2c7219194a7ba
Husdady Mena <husdadymena@gmail.com>
2025-07-10 18:43:17 -0500

    Commit changes

--- src/components/PhotoApp/VideoModal/index.jsx
+++ src/components/PhotoApp/VideoModal/index.jsx
@@ -71,7 +71,7 @@ export default function VideoModal(props) {
 
         {!isCapturing && recordedChunks.length > 0 && !videoUrl && (
           <button onClick={handlePreview} className="btn-preview-video">
-            👁 Preview Video
+            👁 Preview Video1
           </button>
         )}
 
